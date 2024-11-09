e375b053229fd91e3de455e6eda251b0388c9fa9
Nils <nils-stegemann@gmx.de>
2024-11-09 16:11:57 +0100

    feat(localStorage): add note persistence with date conversion
    - Added localStorage support to persist notes across sessions
    - Converted date strings back to Date objects when loading notes from localStorage
    - Updated create, edit, and delete actions to save changes to localStorage

--- src/stores/NoteStore.ts
+++ src/stores/NoteStore.ts
@@ -3,16 +3,13 @@ import Note from "@/types/Note";
 
 export const useNoteStore = defineStore('notebookStore', {
     state: () => ({
-        notes: [
-            { id: '1', title: "Cake", description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", date: new Date(2042, 9, 31), icon: 'home' },
-            { id: '2', title: "Bread", description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", date: new Date(2042, 9, 31), icon: 'check_circle' },
-            { id: '3', title: "Water", description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", date: new Date(2042, 9, 31), icon: 'check_box' },
-            { id: '4', title: "Computer", description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", date: new Date(2042, 9, 31), icon: 'width_wide' },
-        ] as Note[]
+        notes: JSON.parse(localStorage.getItem('notes') || '[]').map((note: any) => ({
+            ...note,
+            date: new Date(note.date) // Convert string date back to Date object
+        })) as Note[]
     }),
     getters: {
         totalCount: state => state.notes.length,
-        // getNoteById: state => (id: string): Note | undefined => state.notes.find((n) => n.id === id)
         getNoteById: state => (id: string): Note | undefined => {
             const note = state.notes.find((n) => n.id === id);
 
@@ -23,6 +20,7 @@ export const useNoteStore = defineStore('notebookStore', {
     actions: {
         createNote(note: Note) {
             this.notes.push(note);
+            this.saveNotes();
         },
         editNote(revisedNote: Note, noteId: string) {
             const note = this.notes.find((n) => n.id === noteId);
@@ -32,10 +30,15 @@ export const useNoteStore = defineStore('notebookStore', {
                 note.description = revisedNote.description;
                 note.date = revisedNote.date;
                 note.icon = revisedNote.icon;
+                this.saveNotes();
             }
         },
         deleteNote(id: string) {
             this.notes = this.notes.filter(n => n.id !== id);
+            this.saveNotes();
+        },
+        saveNotes() {
+            localStorage.setItem('notes', JSON.stringify(this.notes));
         }
     }
 });
